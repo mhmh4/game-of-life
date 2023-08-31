12be8aeef3d56d0650c17637f4b16bad6e0221f4
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-08-31 11:26:46 -0400

    Update settings.js

--- public/settings.js
+++ public/settings.js
@@ -1,6 +1,13 @@
-const CANVAS_WIDTH = 900;
-const CANVAS_HEIGHT = 400;
+// const CANVAS_HEIGHT = 400;
+// const CANVAS_WIDTH = 900;
+
 const CELL_LENGTH = 20;
+
+const CANVAS_HEIGHT =
+  Math.ceil((window.innerHeight * 0.8) / CELL_LENGTH) * CELL_LENGTH;
+const CANVAS_WIDTH =
+  Math.ceil((window.innerWidth * 0.9) / CELL_LENGTH) * CELL_LENGTH;
+
 const DELAY = 200;
 
 export { CANVAS_HEIGHT, CANVAS_WIDTH, CELL_LENGTH, DELAY };
